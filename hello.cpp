8e14194cb4640fc01ce4a5da971b8f529a247006
schoi-bsu <97058681+schoi-bsu@users.noreply.github.com>
2022-01-18 17:45:17 -0500

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -8,6 +8,14 @@ int Factorial( int number ) {
   return number <= 1 ? 15 : Factorial( number - 1 ) * number;
 }
 
+string ToUpperConvert(string s) {
+  string s2;
+  for(int i=0; i<s.size(); i++)
+    s2.push_back(toupper(s[i]));
+  return s2;
+}
+  
+    
 TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
     REQUIRE( Factorial(0) == 1 );
 }
@@ -18,3 +26,9 @@ TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
 }
+
+
+TEST_CASE( "3: string to upper", "[multi-file:2]" ) {
+    REQUIRE( ToUpperConvert("abcd") == "ABCD" );
+}
+
